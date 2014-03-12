b2ac7291f88a5b1a373ea711a3547ce517f04282
Azurras <christopherbell1813@gmail.com>
2014-03-12 11:39:24 -0500

    yep
    look here

---
+++ DegreeAudit/src/degreeaudit/DegreeAudit.java
@@ -0,0 +1,30 @@
+package degreeaudit;
+
+/**
+ * CPSC Degree Audit application.
+ * @author CPSC 2740, Spring 2014
+ */
+public class DegreeAudit {
+
+    public static void main(String[] args) {
+        displayWelcomeMessage();
+        tnguye91();
+        nsoifer();
+    }
+
+    public static void displayWelcomeMessage() {
+        System.out.println("Hello, World!");
+    }
+    
+    public static void tnguye91() {
+        
+    }
+    
+    public static void nsoifer() {
+        
+    }
+
+    public static void cBellIsGreat() {
+        System.out.println("It's true.");
+    }
+}
