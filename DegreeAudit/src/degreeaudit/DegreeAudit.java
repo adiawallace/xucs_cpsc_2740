60f6372c687c90ab4bb35d55fa8cd9055211361c
mtran14xula <mtran14@xula.edu>
2014-03-12 16:26:24 -0500

    Umm commited?
    I'm afraid of commitment.

--- DegreeAudit/src/degreeaudit/DegreeAudit.java
+++ DegreeAudit/src/degreeaudit/DegreeAudit.java
@@ -2,6 +2,7 @@ package degreeaudit;
 
 /**
  * CPSC Degree Audit application.
+ *
  * @author CPSC 2740, Spring 2014
  */
 public class DegreeAudit {
@@ -9,19 +10,24 @@ public class DegreeAudit {
     public static void main(String[] args) {
         displayWelcomeMessage();
         tnguye91();
+        mtran14();
         nsoifer();
     }
 
     public static void displayWelcomeMessage() {
         System.out.println("Hello, World!");
     }
-    
+
     public static void tnguye91() {
-        
+
     }
-    
+
+    public static void mtran14() {
+        System.out.println("Hello, World!");
+    }
+
     public static void nsoifer() {
-        
+
     }
 
     public static void cBellIsGreat() {
